7d12e0593f3b1d2edf56051e2013a9d05da8f34f
Phil Smith <pbsmithtbn@gmail.com>
2020-03-04 01:07:30 -0600

    Extract enzyme setuo to setupTests.js

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,11 +1,8 @@
 import React from "react";
-import Enzyme, { shallow, ShallowWrapper } from "enzyme";
-import EnzymeAdapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
 
 import Congrats from "./Congrats";
 import { findByTestAttr, checkProps } from "../test/testUtils";
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const defaultProps = { success: false };
 
