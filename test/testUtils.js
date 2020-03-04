e8966d5006217cb276bc94ed5dff2b8046b60f2c
Phil Smith <pbsmithtbn@gmail.com>
2020-03-04 00:45:19 -0600

    Add Congrats component and test utils

---
+++ test/testUtils.js
@@ -0,0 +1,9 @@
+/**
+ * Return node(s) with the given data-type attribute.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
+ * @param {string} val - Value of data-test attribute for search.
+ * @returns {ShallowWrapper}
+ */
+export const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+};
