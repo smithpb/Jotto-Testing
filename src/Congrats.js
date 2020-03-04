d13011df1bcb56a5b095a75270f48d2b242737fa
Phil Smith <pbsmithtbn@gmail.com>
2020-03-04 01:02:15 -0600

    Add proptypes for Congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Congrats(props) {
   if (props.success) {
@@ -13,5 +14,9 @@ function Congrats(props) {
     return <div data-test="component-congrats"></div>;
   }
 }
+
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired
+};
 
 export default Congrats;
